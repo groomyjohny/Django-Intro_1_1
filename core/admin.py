27d02b265088a1f9c9b577a2ee3d0d9df07d92d4
groomyjohny <77740043+groomyjohny@users.noreply.github.com>
2022-11-02 12:00:42 +0500

    Added admin for EmergencyServiceModel, verbose names for models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+import core.models
 # Register your models here.
+from core import models
+
+@admin.register(core.models.EmergencyServiceModel)
+class EmergencySeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serviceCode', 'phoneNumber')
--- core/models.py
+++ core/models.py
@@ -4,14 +4,25 @@ from django.db import models
 class EmergencyServiceModel(models.Model):
     name = models.CharField('Название', max_length=128)
     serviceCode = models.IntegerField('Код службы')
-    phoneNumber = models.BigIntegerField('Номер телефона') #TODO: maybe char field?
+    phoneNumber = models.CharField('Номер телефона', max_length=16)
+
+    class Meta:
+        verbose_name = "Экстренная служба"
+        verbose_name_plural = "Экстренные службы"
 
 class ApplicantModel(models.Model):
     fullName = models.CharField('ФИО', max_length=128)
     phoneNumber = models.BigIntegerField('Номер телефона')
     healthState = models.TextField("Состояние здоровья")
 
+    class Meta:
+        verbose_name = "Заявитель"
+        verbose_name_plural = "Заявители"
 class AppealModel(models.Model):
     date = models.DateTimeField("Дата")
     number = models.IntegerField("Номер")
     applicantId = models.ForeignKey(ApplicantModel, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Обращение"
+        verbose_name_plural = "Обращения"

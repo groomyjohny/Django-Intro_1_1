cc4b330f59bb7b573545f2989df87e7d9c8ba105
groomyjohny <77740043+groomyjohny@users.noreply.github.com>
2022-11-07 12:07:04 +0500

    1.5: views first part done

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+import core.views
+
+urlpatterns = [
+    path('views/1', core.views.accident_count_view),
+    path('views/2', core.views.applicant_phone_number_view),
+    path('views/3', core.views.redirect_src_view),
+    path('views/redirect_dst', core.views.redirect_dst_view),
+    path('views/4', core.views.rq_echo_view),
+    path('views/5', core.views.user_data_by_phone_view),
+    path('views/6/<int:uid>', core.views.user_json)
+    ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import json
 
+from django.http import JsonResponse, HttpResponse
+from core import models
+from django.shortcuts import get_object_or_404, redirect, render
 # Create your views here.
+
+def accident_count_view(request):
+    count = models.AccidentModel.objects.count()
+    if count == 0:
+        return HttpResponse(404)
+    return render(request, "views_1.html", context={'count': count})
+
+
+def applicant_phone_number_view(request):
+    a_id = request.GET['id']
+    phone = get_object_or_404(models.ApplicantModel, id=a_id).phone_number
+    return render(request, "views_2.html", context={'phone': phone, 'id': a_id})
+
+
+def redirect_src_view(request):
+    return redirect('/views/redirect_dst')
+
+
+def redirect_dst_view(request):
+    return render(request, 'redirect_target.html')
+
+
+def rq_echo_view(request):
+    return render(request, 'views_4.html', context={'params': request.GET}) # TODO: remake!!!
+
+
+def user_data_by_phone_view(request):
+    phone = request.GET['phone']
+    usr = models.ApplicantModel.objects.filter(phone_number=phone).values()[0]
+
+    return render(request, 'views_5.html', context={'user_dict': usr}) # instead of just passing dict we can form it ourselves
+
+
+def user_json(request, uid):
+    usr = models.ApplicantModel.objects.filter(id=uid).values()[0]
+
+    return JsonResponse({'result' : usr})

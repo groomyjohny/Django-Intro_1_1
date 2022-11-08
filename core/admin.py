c1941d6f44049234214aafb5f8b28074124e9740
groomyjohny <groomyjohny@gmail.com>
2022-11-08 23:06:28 +0500

    1.4: added filters for Applicants

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib import admin
 
 import core.models
@@ -12,9 +14,30 @@ class EmergencyServiceAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 
+class DecadeBornListFilter(admin.SimpleListFilter):
+    title = 'Дата рождения'
+    parameter_name = 'birth_year_range'
+
+    def lookups(self, request, model_admin):
+        return reversed(tuple(
+            (f"{yr},{yr+10}", f"{yr}-{yr+9}") for yr in range(1900, datetime.datetime.now().year+1, 10)
+        ))
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+
+        yr_low, yr_high = [int(i) for i in self.value().split(',')]
+        return queryset.filter(
+            birth_date__gte=datetime.datetime(yr_low, 1, 1),
+            birth_date__lt=datetime.datetime(yr_high, 1, 1)
+        )
+
+
 @admin.register(core.models.ApplicantModel)
 class ApplicantAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'gender', 'birth_date', 'phone_number', 'health_state')
+    list_filter = ('gender', DecadeBornListFilter)
     empty_value_display = '(нет значения)'
 
 

db781ce1e4bc87bdf6152ad27877cdf7ff76cac8
groomyjohny <77740043+groomyjohny@users.noreply.github.com>
2022-11-10 14:41:36 +0500

    Moved all views to views/classed.py

--- core/urls.py
+++ core/urls.py
@@ -1,34 +1,34 @@
 from django.urls import path
-import core.views
+import core.views.classed
 
 urlpatterns = [
-    path('views/1', core.views.AccidentCountView.as_view()),
-    path('views/2', core.views.ApplicantPhoneNumberView.as_view()),
-    path('views/3', core.views.RedirectSrcView.as_view()),
-    path('views/redirect_dst', core.views.RedirectDstView.as_view()),
-    path('views/4', core.views.RequestEchoView.as_view()),
-    path('views/5', core.views.UserDataByPhoneView.as_view()),
-    path('views/6/<int:uid>', core.views.UserJsonView.as_view()),
+    path('views/1', core.views.classed.AccidentCountView.as_view()),
+    path('views/2', core.views.classed.ApplicantPhoneNumberView.as_view()),
+    path('views/3', core.views.classed.RedirectSrcView.as_view()),
+    path('views/redirect_dst', core.views.classed.RedirectDstView.as_view()),
+    path('views/4', core.views.classed.RequestEchoView.as_view()),
+    path('views/5', core.views.classed.UserDataByPhoneView.as_view()),
+    path('views/6/<int:uid>', core.views.classed.UserJsonView.as_view()),
 
-    path('all_applicants', core.views.AllApllicantsView.as_view()),
-    path('all_applicants_numbered', core.views.AllApllicantsNumberedView.as_view()),
-    path('all_accidents', core.views.AllAccidentsView.as_view()),
-    path('all_appeals', core.views.AllAppealsView.as_view()),
+    path('all_applicants', core.views.classed.AllApllicantsView.as_view()),
+    path('all_applicants_numbered', core.views.classed.AllApllicantsNumberedView.as_view()),
+    path('all_accidents', core.views.classed.AllAccidentsView.as_view()),
+    path('all_appeals', core.views.classed.AllAppealsView.as_view()),
 
-    path('add_service', core.views.AddServiceView.as_view()),
-    path('add_applicant', core.views.AddApplicantView.as_view()),
-    path('add_appeal', core.views.AddAppealView.as_view()),
-    path('add_accident', core.views.AddAccidentView.as_view()),
+    path('add_service', core.views.classed.AddServiceView.as_view()),
+    path('add_applicant', core.views.classed.AddApplicantView.as_view()),
+    path('add_appeal', core.views.classed.AddAppealView.as_view()),
+    path('add_accident', core.views.classed.AddAccidentView.as_view()),
 
-    path('edit_service/<int:pk>', core.views.EditServiceView.as_view()),
-    path('edit_applicant/<int:pk>', core.views.EditApplicantView.as_view()),
-    path('edit_appeal/<int:pk>', core.views.EditAppealView.as_view()),
+    path('edit_service/<int:pk>', core.views.classed.EditServiceView.as_view()),
+    path('edit_applicant/<int:pk>', core.views.classed.EditApplicantView.as_view()),
+    path('edit_appeal/<int:pk>', core.views.classed.EditAppealView.as_view()),
 
-    path('filter_applicant', core.views.FilterApplicantView.as_view()),
-    path('filter_appeal', core.views.FilterAppealView.as_view()),
-    path('filter_applicant_name', core.views.FilterApplicantNameView.as_view()),
+    path('filter_applicant', core.views.classed.FilterApplicantView.as_view()),
+    path('filter_appeal', core.views.classed.FilterAppealView.as_view()),
+    path('filter_applicant_name', core.views.classed.FilterApplicantNameView.as_view()),
 
-    path('', core.views.IndexView.as_view()),
-    path('footer', core.views.FooterView.as_view()),
-    path('success', core.views.SuccessView.as_view()),
+    path('', core.views.classed.IndexView.as_view()),
+    path('footer', core.views.classed.FooterView.as_view()),
+    path('success', core.views.classed.SuccessView.as_view()),
     ]
